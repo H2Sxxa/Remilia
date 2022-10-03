292644c095d4b3eb2c93e5ebdea9aee3dab1d1c0
IAXRetailer <retailer673@163.com>
2022-10-03 12:33:33 +0800

    add bypass pixiv client,love from rainchan

--- Remilia/utils/net/pixiv.py
+++ Remilia/utils/net/pixiv.py
@@ -1,4 +1,3 @@
-
 def buildheader(token):
     return {
     'App-OS': 'ios',
@@ -24,4 +23,102 @@ def getHeader(auth:dict):
     use getAuth before it
     '''
     return buildheader(auth["access_token"])
+
+class AioBypass:
+    '''
+    A class comes from https://rainchan.win/2021/08/07/%E5%85%B3%E4%BA%8EPixiv%E5%92%8CSNI%E7%9A%84%E9%82%A3%E4%BA%9B%E4%BA%8B/
+    '''
+    @staticmethod
+    def BypassClient(*args,**kwargs):
+        '''
+```python
+import asyncio
+from Remilia.utils.net import pixiv
+async def test():
+    async with pixiv.AioBypass.BypassClient() as client:
+        async with client.get("https://www.pixiv.net/artworks/100275478") as resp:
+            return await resp.text()
     
+loop=asyncio.get_event_loop()
+cnt=loop.run_until_complete(test())
+print(cnt)
+```
+        '''
+        import ssl,aiohttp
+        ssl_ctx = ssl.SSLContext()
+        ssl_ctx.check_hostname = False
+        ssl_ctx.verify_mode = ssl.CERT_NONE
+        connector = aiohttp.TCPConnector(ssl=ssl_ctx, resolver=AioBypass.ByPassResolver())
+        client = aiohttp.ClientSession(*args,**kwargs,connector=connector)
+        return client
+
+    from aiohttp.abc import AbstractResolver
+    @staticmethod
+    class ByPassResolver(AbstractResolver):
+        from typing import List,Dict,Any
+        async def resolve(self, host: str, port: int, family: int) -> List[Dict[str, Any]]:
+            import socket
+            new_host = host
+            if host == "app-api.pixiv.net":
+                new_host = "www.pixivision.net"
+            if host == "www.pixiv.net":
+                new_host = "www.pixivision.net"
+
+            ips = await self.require_appapi_hosts(new_host)
+            result = []
+
+            for i in ips:
+                result.append({
+                    "hostname": "",
+                    "host": i,
+                    "port": port,
+                    "family": family,
+                    "proto": 0,
+                    "flags": socket.AI_NUMERICHOST | socket.AI_NUMERICSERV,
+                })
+            return result
+
+        async def close(self) -> None:
+            pass
+
+        async def require_appapi_hosts(self, hostname, timeout=3) -> List[str]:
+            """
+            通过 Cloudflare 的 DNS over HTTPS 请求真实的 IP 地址。
+            """
+            import aiohttp,json,re
+            URLS = (
+                "https://cloudflare-dns.com/dns-query",
+                "https://1.0.0.1/dns-query",
+                "https://1.1.1.1/dns-query",
+                "https://[2606:4700:4700::1001]/dns-query",
+                "https://[2606:4700:4700::1111]/dns-query",
+            )
+            params = {
+                "ct": "application/dns-json",
+                "name": hostname,
+                "type": "A",
+                "do": "false",
+                "cd": "false",
+            }
+
+            for url in URLS:
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        async with session.get(url, params=params, timeout=timeout) as rsp:
+                            response = await rsp.text()
+                            obj = json.loads(response)
+                            pattern = re.compile(
+                                "((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(1\d\d|2[0-4]\d|25[0-5]|[1-9]\d|\d)")
+                            result = []
+                            for i in obj["Answer"]:
+                                ip = i["data"]
+
+                                if pattern.match(ip) is not None:
+                                    result.append(ip)
+                            #print(result)
+                            return result
+
+                except Exception as e:
+                    #import traceback
+                    #traceback.print_exc()
+                    pass
