2e054ca3c61105451bbae784ee37778db7a11f52
IAXRetailer <h2sxxa0w0@gmail.com>
2023-04-04 22:05:43 +0800

    cli uitl add async support

---
+++ Remilia/utils/cli/async/__init__.py
@@ -0,0 +1,35 @@
+from typing import Optional, Union
+from prompt_toolkit import Application
+from ..prompts import DT, RT, BasePrompt, CanceledError, DisableColorTransformation, NoAnswer
+from ....lite.v2.ClassMixin import Mixin,NameTransform
+from prompt_toolkit.styles import Style
+
+from asyncio import get_event_loop
+def MakeAsync():
+    @Mixin(BasePrompt)
+    class Mixinto:
+        @NameTransform("_build_application")
+        async def mixin_build_application(self, no_ansi: bool, style: Style) -> Application:
+            return await Application(
+                layout=self._build_layout(),
+                style=self._build_style(style),
+                style_transformation=DisableColorTransformation(no_ansi),
+                key_bindings=self._build_keybindings(),
+                mouse_support=True,
+            ).run_async()
+            
+        async def prompt(
+            self,
+            default: DT = None,
+            no_ansi: bool = False,
+            style: Optional[Style] = None,
+        ) -> Union[DT, RT]:
+            app = await self._build_application(no_ansi=no_ansi, style=style or Style([]))
+            result: RT = get_event_loop().run_until_complete(app.run())
+            if result is NoAnswer:
+                if default is None:
+                    raise CanceledError("No answer selected!")
+                return default
+            else:
+                return result
+    return Mixinto
