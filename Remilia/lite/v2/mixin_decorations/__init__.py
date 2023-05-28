4fcd869ae492f4c9109e37e287ed5488c0c66f9f
IAXRetailer <retailer673@163.com>
2023-05-28 11:16:25 +0800

    Fix Hooker

--- Remilia/lite/v2/mixin_decorations/__init__.py
+++ Remilia/lite/v2/mixin_decorations/__init__.py
@@ -1,6 +1,8 @@
-from typing import List
+from types import MethodType
+from typing import Any, List
 from ....base.rtypes import Pair
 from ....utils.SignParas import ParaFilter
+from ....lite.LiteMixin import gc_Mixin
 from abc import abstractmethod
 class DecorationBase:
     def __call__(self,obj):
@@ -13,26 +15,85 @@ class DecorationBase:
     @abstractmethod
     def warp(self,pair:Pair) -> Pair:
         return Pair(Pair.attr_A,Pair.attr_B)
+
+class ParaHandleBase:
+    @abstractmethod
+    def warp_para(self,arg):pass
+
+class ParaReplacer(ParaHandleBase):
+    def __init__(self,paragen=lambda para:para,condition=lambda para:True) -> None:
+        '''
+        paragen: method(para) -> replace arg
+        condition: wont work until it return True
+        '''
+        self.paragen=paragen
+        self.condition=condition
     
+    def warp_para(self, arg):
+        if self.condition(arg):
+            return self.paragen(arg)
+        else:
+            return arg
+
+class ParaMixin(ParaHandleBase):
+    def __init__(self,mixins:Pair(str,MethodType)=[],condition=lambda para:True,gc=False) -> None:
+        '''
+        mixins: Pair(method_name,Method)
+        condition: wont work until it return True
+        '''
+        self.mixins=mixins
+        self.cd=condition
+        self.gc=gc
+    def warp_para(self, arg):
+        if not self.cd(arg):
+            return arg
+        new_arg=arg
+        for mixin in self.mixins:
+            if self.gc:
+                gc_Mixin(new_arg,mixin.attr_B,mixin.attr_A)
+            else:
+                setattr(new_arg,mixin.attr_A,mixin.attr_B)
+        return new_arg
     
-class ParaHooker:
-    def __init__(self,bfhooker=lambda *_,**__:None,afhooker=lambda para,result,*_,**__:result) -> None:
-        self._bf=bfhooker
-        self._af=afhooker
-        self._para=None
-    def _setpara(self,x):
-        self._para=x
-        
-    #BUG Can't work
-    def __getattr__(self,name):
-        self.bf(self.para)
-        result=getattr(self._para,name)
-        return self.af(self._para,result)
-class Hooker(DecorationBase):
-    def __init__(self,bfhooker=lambda *_,**__:None,afhooker=lambda obj,result,*_,**__:result,parahooker:List[Pair]=[]) -> None:
+class ParaHooker(ParaHandleBase):
+    def __init__(self,hookname="__getattribute__",bfhooker=lambda *_,**__:None,afhooker=lambda result,*_,**__:result,condition=lambda para:True,gc=False) -> None:
+        '''
+        ### via mixin inject target , wont work for some buildin method
+        hookname: hook target(default is all(maybe) )
+        bfhooker: hook before call it
+        afhooker: hook after call it
+        condition: wont work until it return True
+        '''
+        self.hn=hookname
         self.bf=bfhooker
         self.af=afhooker
-        self.ph=parahooker
+        self.cd=condition
+        self.gc=gc
+    def warp_para(self,arg):
+        if not self.cd(arg):
+            return arg
+        new_arg=arg
+        rawattr=getattr(arg,self.hn)
+        def rebuild(*_,**__):
+            self.bf(*_,**__)
+            result=rawattr(*_,**__)
+            return self.af(result,*_,**__)
+        if self.gc:
+            gc_Mixin(new_arg,rebuild,self.hn)
+        else:
+            setattr(new_arg,self.hn,rebuild)
+        return new_arg
+    
+class Hooker(DecorationBase):
+    def __init__(self,bfhooker=lambda *_,**__:None,afhooker=lambda obj,result,*_,**__:result,parahandle:List[Pair]=[]) -> None:
+        '''
+        parahandle : [Pair(the index of para,parahandlebase's subclass instance),...]
+        bfhooker: hook before call it
+        afhooker: hook after call it
+        '''
+        self.bf=bfhooker
+        self.af=afhooker
+        self.ph=parahandle
     def warp(self,pair) -> Pair:
         return Pair(pair.attr_A,self.warpper)
     
@@ -47,14 +108,14 @@ class Hooker(DecorationBase):
             self.bf(self.obj,*args,**kwargs)
             for arg,index in zip(args,range(0,len(args))):
                 default=filter.kwargs[filter.get_index(index).name]
-                if isinstance(default,ParaHooker):
-                    filter.index_put(index,default._setpara(arg))
+                if isinstance(default,ParaHandleBase):
+                    filter.index_put(index,default.warp_para(arg))
                 else:
                     filter.index_put(index,arg)
             for k,v in kwargs.items():
                 default=filter.get_name(k)
-                if isinstance(default,ParaHooker):
-                    filter.kwargs.update({k:default._setpara(v)})
+                if isinstance(default,ParaHandleBase):
+                    filter.kwargs.update({k:default.warp_para(arg)})
                 else:
                     filter.kwargs.update({k:v})
             result=self.obj(**filter.kwargs)
