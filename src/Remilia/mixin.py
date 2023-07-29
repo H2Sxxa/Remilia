4f5a1096ebd04e9bdcd53f21ef4f2cd081fcfe06
IAXRetailer <h2sxxa0w0@gmail.com>
2023-07-29 08:23:20 +0800

    init mixin

--- src/Remilia/mixin.py
+++ src/Remilia/mixin.py
@@ -1,1 +1,73 @@
-class Mixin:pass
+from abc import ABC,abstractmethod
+from enum import Enum
+from types import MethodType
+from typing import Any, List, Type, Union
+from typing_extensions import Self
+
+class At(Enum):
+    BEFORE_INVOKE = -1
+    INVOKE = 0
+    HEAD = 1
+    RETURN = 2
+    AFTER_RETURN = 3
+
+MIXIN_TARGETSCLS="__mixin_targetcls__"
+
+
+class Mixin:
+    target:List[Type]
+    debuginfo:List[Exception]
+    
+    def __init__(self, target: List[Type]) -> None:
+        self.target=target
+        self.debuginfo=[]
+    def __call__(self,mixincls:Type) -> Any:
+        for mname in dir(mixincls):
+            try:
+                obj=getattr(mixincls,mname)
+                if isinstance(obj,MethodType):
+                    setattr(obj,MIXIN_TARGETSCLS,self.target)
+            except Exception as e:
+                self.debuginfo.append(e)
+                
+class MixinMethodType("MethodType"):
+    @property
+    def __mixin_targetcls__(self) -> Type:
+        return None
+class MixinBase(ABC):
+    at:At
+    method:str
+    
+    
+    def init(self,mixinmethod:MethodType):
+        if self.method == None:
+            self.method=mixinmethod.__name__
+        elif isinstance(self.method,str):
+            pass
+        else:
+            self.method=self.method.__name__
+    
+    def cast(self,subtype:"MixinBase",at:At,method:Union[str,MethodType,None]=None) -> MethodType:
+        return subtype().new(at,method).init
+    
+    def new(self,at:At,method:Union[str,MethodType,None]=None) -> Self:
+        self.at=at
+        self.method=method        
+        return self
+    
+    @abstractmethod
+    @staticmethod
+    def withValue(at:At,method:Union[str,MethodType,None]=None):
+        '''
+        if method == None,it will pick up the name of the MixinMethod
+        '''
+        return MixinBase().cast(MixinBase,at,method)
+
+class Inject(MixinBase):pass
+
+class Redirect(MixinBase):
+    pass
+
+
+class OverWrite(MixinBase):
+    pass
--- src/Remilia/shadow.py
+++ src/Remilia/shadow.py
@@ -6,13 +6,15 @@ from uuid import uuid4
 from .base.exceptions import ExistedObjectError, NoSuchMethodError
 from .signs import Signs
 
-SHADOW_SELF="__shadowaccssor_self__"
-SHADOW_MAP="__shadowmethod_map__"
+SHADOW_SELF = "__shadowaccssor_self__"
+SHADOW_MAP = "__shadowmethod_map__"
 
-SelfType=_empty
+SelfType = _empty
+
 
 def getShadowWarpWithCls(cls):
     return Shadow.withCls(cls)
+
 
 class Shadow:
     def init(self, method: MethodType) -> MethodType:
@@ -20,18 +22,18 @@ class Shadow:
         self.method = method
         self.shadowUpdate()
         return method
-    
+
     @staticmethod
-    def withCls(cls:Type) -> Callable[[Type],Callable[[MethodType],MethodType]]:
+    def withCls(cls: Type) -> Callable[[Type], Callable[[MethodType], MethodType]]:
         return Shadow()._withCls(cls)
 
-    def _withCls(self,cls:Type) -> Callable[[MethodType],MethodType]:
-        self.cls=cls
+    def _withCls(self, cls: Type) -> Callable[[MethodType], MethodType]:
+        self.cls = cls
         return self.init
-    
+
     @property
     def shadowName(self) -> str:
-        return self.name + "_%s" % str(uuid4()).replace("-","")
+        return self.name + "_%s" % str(uuid4()).replace("-", "")
 
     def shadowUpdate(self) -> None:
         if not hasattr(self.cls, SHADOW_MAP):
@@ -42,62 +44,86 @@ class Shadow:
         if not shadowmap.__contains__(self.name):
             shadowmap.update({self.name: dict()})
         while True:
-            tmpname=self.shadowName
+            tmpname = self.shadowName
             if not shadowmap.get(self.name).__contains__(tmpname):
-                setattr(self.cls,tmpname,self.method)
-                shadowmap[self.name].update({getattr(self.cls,tmpname):Signs.getParasAsType(self.method)})
+                setattr(self.cls, tmpname, self.method)
+                shadowmap[self.name].update(
+                    {getattr(self.cls, tmpname): Signs.getParasAsType(self.method)}
+                )
                 break
 
+
 class ShadowInvoker:
-    def __init__(self,cls:Type) -> None:
-        self.cls=cls
-    def findAllWithType(self,paratype:List[object]) -> List[MethodType]:
+    def __init__(self, cls: Type) -> None:
+        self.cls = cls
+
+    def findAllWithType(self, paratype: List[object]) -> List[MethodType]:
         shadowmap: Dict[str, Dict[MethodType, List]]
         shadowmap = getattr(self.cls, "__shadowmethod_map__")
-        result=[]
-        for nvmap in [v for _,v in shadowmap.items()]:
-            result.extend([n for n,v in nvmap.items() if v == paratype])
+        result = []
+        for nvmap in [v for _, v in shadowmap.items()]:
+            result.extend([n for n, v in nvmap.items() if v == paratype])
         return result
-    
-    def findFirstWithType(self,paratype:List[object]) -> List[MethodType]:
+
+    def findFirstWithType(self, paratype: List[object]) -> List[MethodType]:
         try:
             return self.findAllWithType(paratype)[0]
         except:
-            raise NoSuchMethodError("can't find method with %s in %s"%(paratype,self.cls))   
-                 
-    def findAll(self,method:Union[str,MethodType],paratype:List[object]) -> List[MethodType]:
+            raise NoSuchMethodError(
+                "can't find method with %s in %s" % (paratype, self.cls)
+            )
+
+    def findAll(
+        self, method: Union[str, MethodType], paratype: List[object]
+    ) -> List[MethodType]:
         shadowmap: Dict[str, Dict[MethodType, List]]
         shadowmap = getattr(self.cls, "__shadowmethod_map__")
-        if not issubclass(method.__class__,str):
-            methodN=method.__name__
+        if not issubclass(method.__class__, str):
+            methodN = method.__name__
         else:
-            methodN=method
-        return [method for method,paras in shadowmap[methodN].items() if paras == paratype]
-        
-    def findFirst(self,method:Union[str,MethodType],paratype:List[object]) -> MethodType:
+            methodN = method
+        return [
+            method for method, paras in shadowmap[methodN].items() if paras == paratype
+        ]
+
+    def findFirst(
+        self, method: Union[str, MethodType], paratype: List[object]
+    ) -> MethodType:
         try:
-            return self.findAll(method,paratype)[0]
+            return self.findAll(method, paratype)[0]
         except:
-            raise NoSuchMethodError("can't find '%s' method with %s in %s"%(method,paratype,self.cls))
-        
-    def invokeAll(self,method:Union[str,MethodType],paratype:List[object],*args,**kwargs) -> List[Any]:
-        return [mtd(*args,**kwargs) for mtd in self.findAll(method,paratype)]
+            raise NoSuchMethodError(
+                "can't find '%s' method with %s in %s" % (method, paratype, self.cls)
+            )
 
-    def invokeFirst(self,method:Union[str,MethodType],paratype:List[object],*args,**kwargs) -> Any:
-        return self.findFirst(method,paratype)(*args,**kwargs)
+    def invokeAll(
+        self, method: Union[str, MethodType], paratype: List[object], *args, **kwargs
+    ) -> List[Any]:
+        return [mtd(*args, **kwargs) for mtd in self.findAll(method, paratype)]
 
-    def invokeAllWithType(self,paratype:List[object],*args,**kwargs) -> List[Any]:
-        return [mtd(*args,**kwargs) for mtd in self.findAllWithType(paratype)]
+    def invokeFirst(
+        self, method: Union[str, MethodType], paratype: List[object], *args, **kwargs
+    ) -> Any:
+        return self.findFirst(method, paratype)(*args, **kwargs)
 
-    def invokeFirstWithType(self,paratype:List[object],*args,**kwargs) -> Any:
-        return self.findFirstWithType(paratype)(*args,**kwargs)
+    def invokeAllWithType(self, paratype: List[object], *args, **kwargs) -> List[Any]:
+        return [mtd(*args, **kwargs) for mtd in self.findAllWithType(paratype)]
+
+    def invokeFirstWithType(self, paratype: List[object], *args, **kwargs) -> Any:
+        return self.findFirstWithType(paratype)(*args, **kwargs)
+
 
 class ShadowAccessor:
-    def __init__(self,cls:Type) -> None:
-        self.cls=cls
-    def setAccessible(self,name:str,force=False) -> None:
+    def __init__(self, cls: Type) -> None:
+        self.cls = cls
+
+    def setAccessible(self, name: str, force=False) -> None:
         if not force:
-            if hasattr(self.cls,name):
-                raise ExistedObjectError("%s has existed object -> %s" % (self.cls.__name__,name))
-        setattr(self.cls,name,lambda _:getattr(_,"_%s%s" % (self.cls.__name__,name)))
-        setattr(self.cls,name,property(getattr(self.cls,name)))
+            if hasattr(self.cls, name):
+                raise ExistedObjectError(
+                    "%s has existed object -> %s" % (self.cls.__name__, name)
+                )
+        setattr(
+            self.cls, name, lambda _: getattr(_, "_%s%s" % (self.cls.__name__, name))
+        )
+        setattr(self.cls, name, property(getattr(self.cls, name)))
